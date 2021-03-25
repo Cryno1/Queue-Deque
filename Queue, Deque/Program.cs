39ce3c1f87e6bc3a62ce2b0b5880bd489a0fb0f0
Dzenobo <gggss11113@gmail.com>
2021-03-25 13:54:45 +0600

    Добавьте файлы проекта.

---
+++ Queue, Deque/Program.cs
@@ -0,0 +1,26 @@
+﻿using Queue__Deque.Model;
+using System;
+
+namespace Queue__Deque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var easyDeque = new EasyDeque<int>();
+            easyDeque.PushBack(1);
+            easyDeque.PushFront(3);
+            easyDeque.PushBack(4);
+            easyDeque.PushFront(2);
+
+            Console.WriteLine(easyDeque.PeekBack());
+            Console.WriteLine(easyDeque.PeekFront());
+            Console.WriteLine(easyDeque.PopFront());
+            Console.WriteLine(easyDeque.PeekFront());
+            Console.WriteLine(easyDeque.PopBack());
+            Console.WriteLine(easyDeque.PeekBack());
+
+            Console.ReadLine();
+        }
+    }
+}
